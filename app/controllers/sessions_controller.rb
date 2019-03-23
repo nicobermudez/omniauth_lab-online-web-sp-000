9d95fbb424ac0a1e97c3315c0e90074fed5af431
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-23 20:10:36 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,6 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_by(:uid => auth['uid']) do |user|
       user.name = auth['info']['name']
-      user.email = auth['info']['name']
-      user.image = auth['info']['image']
     end
     session[:user_id] = @user.try(:id)
     render 'welcome/home'
